533ceafcbbe3280f540783e607582cbab1e0d53a
bishoy <bishoy@localhost.localdomain>
2019-12-19 18:50:08 -0500

    modifications to analysisCsI.sh script file

--- analysisCsI.sh
+++ analysisCsI.sh
@@ -4,4 +4,4 @@ echo "**************| BEGIN ANALYSIS ROUTINE |******************"
 echo "**********************************************************"
 echo "Enter run number to be analyzed"
 read runNo
-nice ./bin/cooker ../trekCkr/recipe/CsI/marinate.xml $path_cooked/cookedRun${runNo}.root $path_marinate/marinRun${runNo}.root
+nice ./bin/cooker ../recipe/CsI/marinate.xml $path_cooked/cookedRun${runNo}.root $path_marinate/marinRun${runNo}.root
