5280ca2b76ba3369a1ef8eddb4dff4bc65d9f52f
bishoy <bishoy@localhost.localdomain>
2019-12-23 14:32:24 -0500

    Added the possibility of using tof1 multiplicity in clusterAna.cpp

--- src/plugins/analysis/covfefe/src/clusterAna.cpp
+++ src/plugins/analysis/covfefe/src/clusterAna.cpp
@@ -45,17 +45,19 @@ Long_t covfefe::process_clust(){
       double bz=piPpz/E;
       double b2=bx*bx+by*by+bz*bz;
       double gamma=1.0/std::sqrt(1.0-b2);
-      clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,0);
+      //clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,0);
       clustEval->fillHistos(prim2lv.M2(),0);
       //clustEval->fillHistos(clsmar->cpid1theta,clsmar->cpid1phi,clsmar->cpid2theta,clsmar->cpid2phi,0,0);
       clustEval->fillMltp(clsmar->clusterM);
+      // eval only 2 clusters
       if(clsmar->clusterM==2 && clsmar->E_prim2>0.02){
-        clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,1);
-        clustEval->fillHistos(prim2lv.M2(),1);
+        clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,0);
+        clustEval->fillHistos(prim2lv.M2(),0);
 	//std::cout<<" Checking the momentum of pi+ =>"<<P<<"\n";
-	if(prim2lv.M()>= .07 && prim2lv.M()<=.16){
-          clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,2);
-          clustEval->fillHistos(prim2lv.M2(),2);
+	// check that TOF1 multiplicity is greater than or equal to 2
+	if(clsmar->extraTOF1>=2){
+          clustEval->fillHistos(clsmar->M_prim2,clsmar->clCosTheta,clsmar->E_prim2,clsmar->prCosTheta,1);
+          clustEval->fillHistos(prim2lv.M2(),1);
 	  //std::cout<<" ... Mass2: "<<prim2lv.M2()<<std::endl;
 	  //std::cout<<" Px, Py, Pz: ["<<clsmar->prim2px<<", "<<clsmar->prim2py<<", "<<clsmar->prim2pz<<"]\n";
 	}
