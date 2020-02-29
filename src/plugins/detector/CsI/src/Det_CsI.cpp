44dfc201f3983438d9c03f952eb77e62e27a7d6e
bishoy <bishoy@localhost.localdomain>
2020-02-29 18:44:29 -0500

    additional checking on the CsI timing in gap 7

--- src/plugins/detector/CsI/src/Det_CsI.cpp
+++ src/plugins/detector/CsI/src/Det_CsI.cpp
@@ -35,7 +35,7 @@ Long_t Det_CsI::setIdCsI(map<IdCsI,UInt_t> & map){
 }
 */
 Long_t Det_CsI::histos(){
-  h1Ch=new TH1D("h1Ch","channel7",10,-1,9);
+  h1Ch=new TH1D("h1Ch","channel7",34,-17.5,16.5);
   return 0;
 }
 
@@ -215,9 +215,9 @@ Long_t Det_CsI::process(){
         std::cout<<"============> Hell Ya this is an empty module (?) <==============\n";
         std::cout<<"============> Hell Ya this is an empty module (?) <==============\n";
 	if(iFB==1)
-	  h1Ch->Fill(iFB);
+	  h1Ch->Fill(iModule);
 	if(iUD==0)
-	  h1Ch->Fill(iUD);
+	  h1Ch->Fill(-1*iModule);
       }
       //    if(iCsI==144 || iCsI==400||iCsI==656||iCsI==166||iCsI==128) continue;
       //SingleCsI myCsI(treeRaw->runNo,myEvent,iModule);
