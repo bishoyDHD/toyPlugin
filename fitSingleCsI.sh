da5432a41f05eac639c621e307eb862e465ae38b
Bishoy Dongwi <bishoy@trekvm1.triumf.ca>
2020-04-21 10:55:12 -0700

    update fitSingleCsI.sh script

--- fitSingleCsI.sh
+++ fitSingleCsI.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #nice ./bin/visco ../recipe/CsI/test.xml $path_decoded/hdw_3vf48.root $path_cooked/calib47Runs.root
-echo "Please enter run number to be sanity checked"
-read num;
-nice ./bin/visco ../recipe/CsI/test.xml $path_decoded/hdw_${num}.root $path_cooked/testRealTimeRun${num}.root
+#echo "Please enter run number to be sanity checked"
+#read num;
+nice ./bin/visco ../recipe/CsI/test.xml $path_calibData/calibRuns1.root $path_cooked/calib65timeRuns_1.root
+#nice ./bin/visco ../recipe/CsI/test.xml $path_decoded/calibRuns.root $path_cooked/calibTimeRun.root
+#nice ./bin/visco ../recipe/CsI/test.xml $path_decoded/hdw_${num}.root $path_cooked/testRealTimeRun${num}.root
 #---> For target calib files
 #nice ./bin/visco ../recipe/CsI/test.xml $path_decoded/hdw_${num}.root:$path_converted/run${num}tgtConv.root $path_cooked/sanityRun${num}.root
